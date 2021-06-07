abd667191542f9cf366020dd5905b2411da6f974
Sudhanshu Tripathi <65442845+2sudhanhu@users.noreply.github.com>
2021-06-07 21:49:32 +0530

    test8

--- sudhanshu.py
+++ sudhanshu.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello Sudhanshu! test7 </h1>"
+    return "<h1 style='color:blue'>Hello Sudhanshu! test8 </h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
